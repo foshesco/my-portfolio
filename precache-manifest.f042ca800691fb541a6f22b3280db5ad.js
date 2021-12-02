2da06541dc1f35c8c279836dd3591f04f8bf3028
EF <foshesco@gmail.com>
2021-12-01 20:22:18 -0600

    Updates

---
+++ precache-manifest.f042ca800691fb541a6f22b3280db5ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbfc69b049b36d708491",
+    "url": "/static/css/main.a347e591.chunk.css"
+  },
+  {
+    "revision": "dbfc69b049b36d708491",
+    "url": "/static/js/main.dbfc69b0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0efb80e9fa6c4bc7ddf8",
+    "url": "/static/js/2.0efb80e9.chunk.js"
+  },
+  {
+    "revision": "6795a3472e757d0a5a70e404e3ba393a",
+    "url": "/static/media/golf.6795a347.PNG"
+  },
+  {
+    "revision": "e6fd1867ec206ea3365c017234010337",
+    "url": "/static/media/acftpreview.e6fd1867.gif"
+  },
+  {
+    "revision": "624becba6b6315592ecdcf9ca385b495",
+    "url": "/static/media/golfpreview.624becba.gif"
+  },
+  {
+    "revision": "212dbfe1812aeee0c475eb88d576042a",
+    "url": "/index.html"
+  }
+];
