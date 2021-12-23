26cc75a14e25cc045c17c551c475ddd0f098ff58
EF <foshesco@gmail.com>
2021-12-22 22:23:34 -0600

    header ui fix

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,21 +7,19 @@ const Home = () => {
         <div className="homeContainer">
             <div className="homeContainerCont">
                 <div>
-                    {/* <h2 className="animated-intro">
-                        <span>Welcome</span>
-                        <span>Bienvenidos</span>
-                        <span>வரவேற்பு</span>
-                        <span>欢迎</span>
-                        <span>أهلا بك</span>
-                    </h2>
-                    <p className="intro">A creative and detailed individual with the ability to develop effective and efficient solutions with zero tolerance for errors. Looking to join a progressive tech company where my technical skills will be fully harnessed.</p> */}
                     <div className="home-section animated animatedFadeInUp fadeInUp-five">
                         <span>
                             <h2 className="home-hello">Hi, I'm Erik.</h2>
                         </span>
                     </div>
                     <div className="home-section animated animatedFadeInUp fadeInUp-six">
-                        <p className="home-intro">I'm a front-end developer specializing in building exceptional digital experiences.</p>
+                        <span className="animated-intro">I'm a front-end developer specializing in building exceptional digital
+                            <span>&nbsp;experiences.</span>
+                            <span>&nbsp;applications.</span>
+                            <span>&nbsp;pages.</span>
+                            <span>&nbsp;programs.</span>
+                            <span>&nbsp;things.</span>
+                        </span>
                     </div>
                     <div className="animated animatedFadeInUp fadeInUp-seven">
                         <a href="#contact">
